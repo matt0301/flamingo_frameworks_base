2ceac9b49ae6c9604b161eeef4659bd220db0d42
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-04-19 14:31:41 +0300

    gmscompat: conditionally stub out BluetoothAdapter.get{Address,Name}()

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1287,6 +1287,12 @@ public final class BluetoothAdapter {
             android.Manifest.permission.LOCAL_MAC_ADDRESS,
     })
     public String getAddress() {
+        if (GmsCompat.isEnabled()){
+            if (!GmsCompat.hasPermission(android.Manifest.permission.BLUETOOTH_CONNECT)) {
+                return DEFAULT_MAC_ADDRESS;
+            }
+        }
+
         try {
             return mManagerService.getAddress(mAttributionSource);
         } catch (RemoteException e) {
@@ -1305,6 +1311,12 @@ public final class BluetoothAdapter {
     @RequiresBluetoothConnectPermission
     @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
     public String getName() {
+        if (GmsCompat.isEnabled()) {
+            if (!GmsCompat.hasPermission(android.Manifest.permission.BLUETOOTH_CONNECT)) {
+                return null;
+            }
+        }
+
         try {
             return mManagerService.getName(mAttributionSource);
         } catch (RemoteException e) {
