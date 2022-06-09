AOSP-Krypton/frameworks_base

SystemUI: allow disabling qs on secure lockscreen[1/2]
* This does not prevent pulling down notification shade, qs will be entirely hidden even if you do and that's it.

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -53,6 +53,7 @@ import com.android.systemui.statusbar.notification.stack.StackStateAnimator;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.phone.NotificationsQuickSettingsContainer;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
+import com.android.systemui.statusbar.policy.SecureLockscreenQSDisabler;
 import com.android.systemui.util.InjectionInflationController;
 import com.android.systemui.util.LifecycleFragment;
 import com.android.systemui.util.Utils;
@@ -99,6 +100,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private final MediaHost mQqsMediaHost;
     private final QSFragmentComponent.Factory mQsComponentFactory;
     private final QSTileHost mHost;
+    private final SecureLockscreenQSDisabler mSecureLockscreenQSDisabler;
     private boolean mShowCollapsedOnKeyguard;
     private boolean mLastKeyguardAndExpanded;
     /**
@@ -143,7 +145,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
             @Named(QUICK_QS_PANEL) MediaHost qqsMediaHost,
             KeyguardBypassController keyguardBypassController,
             QSFragmentComponent.Factory qsComponentFactory, FeatureFlags featureFlags,
-            FalsingManager falsingManager, DumpManager dumpManager) {
+            FalsingManager falsingManager, DumpManager dumpManager,
+            SecureLockscreenQSDisabler secureLockscreenQSDisabler) {
         mRemoteInputQuickSettingsDisabler = remoteInputQsDisabler;
         mInjectionInflater = injectionInflater;
         mCommandQueue = commandQueue;
@@ -158,6 +161,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mBypassController = keyguardBypassController;
         mStatusBarStateController = statusBarStateController;
         mDumpManager = dumpManager;
+        mSecureLockscreenQSDisabler = secureLockscreenQSDisabler;
     }
 
     @Override
@@ -352,6 +356,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
             return;
         }
         state2 = mRemoteInputQuickSettingsDisabler.adjustDisableFlags(state2);
+        state2 = mSecureLockscreenQSDisabler.adjustDisableState(state2);
 
         final boolean disabled = (state2 & DISABLE2_QUICK_SETTINGS) != 0;
         if (disabled == mQsDisabled) return;

packages/SystemUI/src/com/android/systemui/statusbar/phone/SecureLockscreenQSDisabler.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy
+
+import android.app.StatusBarManager
+import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.UserHandle
+import android.provider.Settings.Secure.SECURE_LOCKSCREEN_QS_DISABLED
+
+import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.CommandQueue
+import com.android.systemui.statusbar.policy.KeyguardStateController
+import com.android.systemui.util.settings.SecureSettings
+
+import javax.inject.Inject
+
+@SysUISingleton
+class SecureLockscreenQSDisabler @Inject constructor(
+    private val context: Context,
+    private val commandQueue: CommandQueue,
+    private val secureSettings: SecureSettings,
+    private val keyguardStateController: KeyguardStateController,
+    @Main handler: Handler,
+) {
+
+    private var disableQSOnSecureLockscreen: Boolean = shouldDisableQS()
+
+    init {
+        val settingsObserver = object: ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean) {
+                disableQSOnSecureLockscreen = shouldDisableQS()
+                recomputeDisableFlags()
+            }
+        }
+        secureSettings.registerContentObserverForUser(SECURE_LOCKSCREEN_QS_DISABLED,
+            settingsObserver, UserHandle.USER_ALL)
+    }
+
+    fun adjustDisableState(state2: Int): Int {
+        return if (disableQSOnSecureLockscreen &&
+                !keyguardStateController.isUnlocked()) {
+            state2 or StatusBarManager.DISABLE2_QUICK_SETTINGS
+        } else {
+            state2
+        }
+    }
+
+    private fun shouldDisableQS(): Boolean =
+        secureSettings.getIntForUser(SECURE_LOCKSCREEN_QS_DISABLED,
+            0, UserHandle.USER_CURRENT) == 1
+
+    private fun recomputeDisableFlags() {
+        commandQueue.recomputeDisableFlags(context.displayId, true /** animate */)
+    }
+}

matt0301/flamingo_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -54,6 +54,7 @@ import com.android.systemui.statusbar.notification.stack.StackStateAnimator;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
+import com.android.systemui.statusbar.policy.SecureLockscreenQSDisabler;
 import com.android.systemui.util.LifecycleFragment;
 import com.android.systemui.util.Utils;
 
@@ -102,6 +103,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private final QSFragmentComponent.Factory mQsComponentFactory;
     private final QSFragmentDisableFlagsLogger mQsFragmentDisableFlagsLogger;
     private final QSTileHost mHost;
+    private final SecureLockscreenQSDisabler mSecureLockscreenQSDisabler;
     private boolean mShowCollapsedOnKeyguard;
     private boolean mLastKeyguardAndExpanded;
     /**
@@ -152,7 +154,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
             KeyguardBypassController keyguardBypassController,
             QSFragmentComponent.Factory qsComponentFactory,
             QSFragmentDisableFlagsLogger qsFragmentDisableFlagsLogger,
-            FalsingManager falsingManager, DumpManager dumpManager) {
+            FalsingManager falsingManager, DumpManager dumpManager,
+            SecureLockscreenQSDisabler secureLockscreenQSDisabler) {
         mRemoteInputQuickSettingsDisabler = remoteInputQsDisabler;
         mCommandQueue = commandQueue;
         mQsDetailDisplayer = qsDetailDisplayer;
@@ -166,6 +169,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mBypassController = keyguardBypassController;
         mStatusBarStateController = statusBarStateController;
         mDumpManager = dumpManager;
+        mSecureLockscreenQSDisabler = secureLockscreenQSDisabler;
     }
 
     @Override
@@ -361,6 +365,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         }
         int state2BeforeAdjustment = state2;
         state2 = mRemoteInputQuickSettingsDisabler.adjustDisableFlags(state2);
+        state2 = mSecureLockscreenQSDisabler.adjustDisableFlags(state2);
 
         mQsFragmentDisableFlagsLogger.logDisableFlagChange(
                 /* new= */ new DisableState(state1, state2BeforeAdjustment),

packages/SystemUI/src/com/android/systemui/statusbar/phone/SecureLockscreenQSDisabler.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy
+
+import android.app.StatusBarManager
+import android.content.Context
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.UserHandle
+import android.provider.Settings.Secure.SECURE_LOCKSCREEN_QS_DISABLED
+
+import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.statusbar.CommandQueue
+import com.android.systemui.statusbar.policy.KeyguardStateController
+import com.android.systemui.util.settings.SecureSettings
+
+import javax.inject.Inject
+
+@SysUISingleton
+class SecureLockscreenQSDisabler @Inject constructor(
+    private val context: Context,
+    private val commandQueue: CommandQueue,
+    private val secureSettings: SecureSettings,
+    private val keyguardStateController: KeyguardStateController,
+    @Main handler: Handler,
+) {
+
+    private var disableQSOnSecureLockscreen: Boolean = shouldDisableQS()
+
+    init {
+        val settingsObserver = object: ContentObserver(handler) {
+            override fun onChange(selfChange: Boolean) {
+                disableQSOnSecureLockscreen = shouldDisableQS()
+                recomputeDisableFlags()
+            }
+        }
+        secureSettings.registerContentObserverForUser(SECURE_LOCKSCREEN_QS_DISABLED,
+            settingsObserver, UserHandle.USER_ALL)
+    }
+
+    fun adjustDisableFlags(state2: Int): Int {
+        return if (disableQSOnSecureLockscreen &&
+                !keyguardStateController.isUnlocked()) {
+            state2 or StatusBarManager.DISABLE2_QUICK_SETTINGS
+        } else {
+            state2
+        }
+    }
+
+    private fun shouldDisableQS(): Boolean =
+        secureSettings.getIntForUser(SECURE_LOCKSCREEN_QS_DISABLED,
+            0, UserHandle.USER_CURRENT) == 1
+
+    private fun recomputeDisableFlags() {
+        commandQueue.recomputeDisableFlags(context.displayId, true /** animate */)
+    }
+}