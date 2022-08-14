3bce3f6a1b385c7412350e23f103d33bd8694602
jhonboy121 <alfredmathew05@gmail.com>
2022-08-14 23:58:23 +0530

    SystemUI: AmbientDisplayTile: fix state in secondary users and cleanup
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
@@ -18,43 +18,36 @@
 package com.android.systemui.qs.tiles;
 
 import android.content.Intent;
+import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Build;
-import android.os.SystemProperties;
 import android.provider.Settings;
-import android.provider.Settings.Secure;
 import android.service.quicksettings.Tile;
-import android.text.TextUtils;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
-
-import com.android.systemui.plugins.qs.QSTile.BooleanState;
-import com.android.systemui.qs.QSHost;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
-import com.android.systemui.qs.SecureSetting;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.SecureSetting;
 import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.util.settings.SecureSettings;
-
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import javax.inject.Inject;
 
 /** Quick settings tile: Ambient Display **/
 public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
 
-    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_ambient_display);
-
-    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
+    private static final Intent LOCK_SCREEN_SETTINGS = new Intent("android.settings.LOCK_SCREEN_SETTINGS");
 
     private final SecureSetting mSetting;
 
@@ -73,7 +66,7 @@ public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
         super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
 
-        mSetting = new SecureSetting(secureSettings, mHandler, Secure.DOZE_ENABLED) {
+        mSetting = new SecureSetting(secureSettings, mHandler, Settings.Secure.DOZE_ENABLED) {
             @Override
             protected void handleValueChanged(int value, boolean observedChange) {
                 handleRefreshState(value);
@@ -83,16 +76,16 @@ public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
 
     @Override
     public boolean isAvailable() {
-        String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
-        if (TextUtils.isEmpty(name)) {
-            name = mContext.getString(com.android.internal.R.string.config_dozeComponent);
-        }
-        return !TextUtils.isEmpty(name);
+        final AmbientDisplayConfiguration config = new AmbientDisplayConfiguration(mContext);
+        return config.pulseOnNotificationAvailable();
     }
 
     @Override
     public BooleanState newTileState() {
-        return new BooleanState();
+        final BooleanState state = new BooleanState();
+        state.label = getTileLabel();
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_ambient_display);
+        return state;
     }
 
     @Override
@@ -103,23 +96,18 @@ public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return DISPLAY_SETTINGS;
+        return LOCK_SCREEN_SETTINGS;
     }
 
     private void setEnabled(boolean enabled) {
-        Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.DOZE_ENABLED,
-                enabled ? 1 : 0);
+        mSetting.setValue(enabled ? 1 : 0);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         final int value = arg instanceof Integer ? (Integer) arg : mSetting.getValue();
-        final boolean enable = value != 0;
-        state.value = enable;
-        state.label = mContext.getString(R.string.quick_settings_ambient_display_label);
-        state.icon = mIcon;
-        if (enable) {
+        state.value = value != 0;
+        if (state.value) {
             state.contentDescription =  mContext.getString(
                     R.string.accessibility_quick_settings_ambient_display_on);
             state.state = Tile.STATE_ACTIVE;
